ce15239bce992213868ccb440a260703e58ad3d1
xyrc1 <75494811+xyrc1@users.noreply.github.com>
2026-01-04 09:39:24 +0000

    fixed literally none of this working

--- patches/technology.lua
+++ patches/technology.lua
@@ -1,4 +1,11 @@
 local t = data.raw.technology
+
+local function add_preqs(tech, preqs)
+    for _,v in pairs(preqs) do
+        table.insert(t[tech].prerequisites, v)
+    end
+end
+
 if settings.startup['xy-paracelsin-tech-card'].value then
     if settings.startup['xy-paracelsin-tech-card-endgame'].value and mods['Paracelsin'] then
         table.insert(t['kr-singularity-lab'].unit.ingredients, {'galvanization-science-pack', 1})
@@ -16,60 +23,68 @@ end
 if settings.startup['xy-advanced-tank-expensive-research'].value then
     t['kr-advanced-tank'].unit.count = 2500
     table.insert(t['kr-advanced-tank'].unit.ingredients, {'metallurgic-science-pack', 1})
-    table.insert(t['kr-advanced-tank'].unit.ingredients, {'electromagnetic-science-pack', 1})
     table.insert(t['kr-advanced-tank'].unit.ingredients, {'agricultural-science-pack', 1})
+    table.insert(t['kr-advanced-tank'].unit.ingredients, {'electromagnetic-science-pack', 1})
+    add_preqs('kr-advanced-tank', {'metallurgic-science-pack','agricultural-science-pack','electromagnetic-science-pack'})
 end
 
--- recode this entire setting eventually (this will never happen)
 if settings.startup['xy-tech-inflation'].value then
     -- increase the cost of all mid-late-endgame techs, starting after the first three vanilla planets
     -- does not affect Moshine as that is unrelated
+
     local hardcoded_multi = { -- overrides the sweeping changes
-        ['planet-discovery-aquilo'] = 10,
-        ['planet-discovery-maraxsis'] = 10,
-        ['planet-discovery-paracelsin'] = 9,
+        ['planet-discovery-aquilo'] = 8,
+        ['planet-discovery-maraxsis'] = 8,
+        ['planet-discovery-paracelsin'] = 8,
         ['planet-discovery-moshine'] = 4,
         ['planet-discovery-secretas'] = 30,
         ['legendary-quality'] = 15,
-        ['quantum-processor'] = 15,
+        ['quantum-processor'] = 30,
         ['kr-intergalactic-transceiver'] = 50, -- vanilla megabase could still do this in 5 minutes lol
         ['kr-advanced-card'] = 2,
+        ['biolab'] = 2,
     }
-    local multi_adv = 2
-    local multi_MAE = 5
-    local multi_cryo_galv_hydr = 7
-    local multi_matt = 10
-    local multi_sing = 15
-    local multi_gold = 20
-    local multi_prometh = 40
+    local general_multi = { -- only applies the highest value, will make this a setting eventually but might be clutter? could organize into sections
+        ['promethium-science-pack'] = 25,
+        ['golden-science-pack'] = 20,
+        ['kr-singularity-tech-card'] = 15,
+        ['kr-matter-tech-card'] = 10,
+        ['cryogenic-science-pack'] = 7,
+        ['galvanization-science-pack'] = 7,
+        ['hydraulic-science-pack'] = 7,
+        ['kr-advanced-tech-card'] = 2,
+        ['MAE-cards'] = 5,
+    }
 
     for _,tech in pairs(t) do
-        if tech.upgrade then goto continue end
+        if (tech.upgrade) or (not tech.unit) or (not tech.unit.count) then goto continue end
 
         if hardcoded_multi[tech.name] then
             tech.unit.count = tech.unit.count * hardcoded_multi[tech.name]
         else
             -- general application
             local used_cards = {}
-            for _,card in tech.unit.ingredients do
+            local highest_multi = 1
+            local MAE = 0 -- as in metallurgic, agricultural, and electromagnetic science. count up and if this is three then apply
+            for _,card in pairs(tech.unit.ingredients) do
                 table.insert(used_cards, card[1]) -- 1-indexed lists
             end
-            if used_cards['promethium-science-pack'] then
-                tech.unit.count = tech.unit.count * multi_prometh
-            elseif used_cards['golden-science-pack'] then
-                tech.unit.count = tech.unit.count * multi_gold
-            elseif used_cards['kr-singularity-tech-card'] then
-                tech.unit.count = tech.unit.count * multi_sing
-            elseif used_cards['kr-matter-tech-card'] then
-                tech.unit.count = tech.unit.count * multi_matt
-            elseif used_cards['cryogenic-science-pack'] or used_cards['galvanization-science-pack'] or used_cards['hydraulic-science-pack'] then
-                tech.unit.count = tech.unit.count * multi_cryo_galv_hydr
-            elseif used_cards['metallurgic-science-pack'] and used_cards['electromagnetic-science-pack'] and used_cards['agricultural-science-pack'] then
-                tech.unit.count = tech.unit.count * multi_MAE
-            elseif used_cards['kr-advanced-tech-card'] then
-                tech.unit.count = tech.unit.count * multi_adv
+            for _,v in pairs(used_cards) do
+                if v == 'agricultural-science-pack' or v == 'metallurgic-science-pack' or v == 'electromagnetic-science-pack' then
+                    MAE = MAE + 1
+                end
+                local card_multi = general_multi[v] or -1
+                if highest_multi < card_multi then
+                    highest_multi = card_multi
+                end
             end
+            if highest_multi == 1 and MAE == 3 then
+                highest_multi = general_multi['MAE-cards']
+            end
+
+            tech.unit.count = tech.unit.count * highest_multi
         end
+
         ::continue::
     end
 end
