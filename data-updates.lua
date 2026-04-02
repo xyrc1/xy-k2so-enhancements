4b3f10e60dea85f5d554ecd99b9224204276aa7e
xyrc1 <75494811+xyrc1@users.noreply.github.com>
2026-04-02 06:50:42 +0000

    update

--- data-updates.lua
+++ data-updates.lua
@@ -1,7 +1,6 @@
 -- fixes; these shouldn't be turned off
 require('fixes.item')
 require('fixes.tech_cards')
--- perhaps poorly named; "updates" are larger scale patches over entire mods
-require('updates.outer-rim')
+
 -- patches; optional but recommended
 require('patches.space-connection')
